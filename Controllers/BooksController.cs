f0a2e4340d619529e92ec544e5875d29731317ea
munchzuu <81302466+munchzuu@users.noreply.github.com>
2021-03-24 11:24:16 +0000

    Update BooksController.cs

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,31 +1,67 @@
-﻿using System.Collections.Generic;
+using Library.Repository;
+using System.Linq;
 using System.Net;
 using System.Web.Http;
 
 namespace Library.Controllers
 {
-    [RoutePrefix("api/books")]
+    [System.Web.Mvc.RoutePrefix("api/books")]    
     public class BooksController : ApiController
     {
-        /*
-		The following GET methods are expected on the api/books controller:
+        BooksRepository bookRepository = new BooksRepository();
+        /// <summary>
+        /// Book Controller constructor
+        /// </summary>
+        public BooksController()
+        { }
+        /// <summary>
+        /// Method to get books
+        /// </summary>
+        /// <returns></returns>
+        public IHttpActionResult Get()
+        {            
+            var books = bookRepository.GetBookNames(); 
+            if(books == null)
+            {
+                return NotFound();
+            }
+            return Json(books.ToList());
+        }
+        /// <summary>
+        /// Get Word counts per book 
+        /// </summary>
+        /// <param name="Id"></param> Id is primary key of the Book class
+        /// <returns></returns>
+        public IHttpActionResult Get(int Id)
+        {
 
-			1. GET api/books
-				Returns a list of Ids & Titles for all the books in the Resources folder
-				Titles should be the name of the filename, minus the extension.
-				The Id should be unique.
-
-			2. GET api/books/{Id}
-				Returns a list of the most common 10 words (min 5 letters) and how many times they occur in the specified book.
-				When parsing, whitespace, linefeeds and punctiation should be ignored, and words matched case-insensitively (e.g. "the" and "The" and "THE" will be returned as "The"=3)
-				Words should be returned in capital case (e.g. Word), and the list should be sorted in decreasing incidence.
-
-			3. GET api/books/{Id}?query={query}
-				Returns a list of all words which start with the specified string (min 3 letters) and the number of times they occur within the specified book.
-				Case matching should be insensitive.
-
-		The logic for these calls should largely be encapsulated in other classes. This should make it easier to Unit Test those classes
-		to validate the expected behaviour.
-		*/
+            var wordCounts = bookRepository.GetWordCounts(Id);
+            if (wordCounts == null)
+            {
+                return NotFound();
+            }
+            return Json(wordCounts);
+        }
+        /// <summary>
+        /// Get search words
+        /// </summary>
+        /// <param name="Id"></param> Book ID
+        /// <param name="query"></param> Word to search in the book
+        /// <returns></returns>
+        public IHttpActionResult Get(int Id, [FromUri] string query)
+        {
+            if (query.Length >= 3)
+            {
+                var wordCounts = bookRepository.SearchWordCounts(Id, query);
+                if (wordCounts == null || wordCounts.Count() == 0)
+                {
+                    return NotFound();
+                }
+                return Json(wordCounts);
+            }
+            else
+                return StatusCode(HttpStatusCode.LengthRequired);                
+            
+        }
     }
 }
